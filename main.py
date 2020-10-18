2be4169f9b39474cc97f0ec0e750dd3680f8818c
Alex Iankoulski <alex_iankoulski@yahoo.com>
2020-10-18 23:34:45 +0000

    Reference cached data in current folder

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ import pandas as pd
 def main(argv):
     print("Running renewable energy project ... ")
     
-    data_path = '/tmp/data.csv'
+    data_path = './data.csv'
     if (len(argv)>0 and ('--wrangle' in argv or '-w' in argv)):
         data_path = data.wrangle()
     
-    preprocessed_data_path = '/tmp/preprocessed_data.csv'
+    preprocessed_data_path = './preprocessed_data.csv'
     if (len(argv)>0 and ('--preprocess' in argv or '-p' in argv)):
         preprocessed_data_path = data.preprocess(data_path)
 
-    train_data_path = '/tmp/train_data.csv'
-    test_data_path = '/tmp/test_data.csv'    
+    train_data_path = './train_data.csv'
+    test_data_path = './test_data.csv'    
     len_train, len_test = data.split(preprocessed_data_path, 70, train_data_path, test_data_path)
     
     prophet_rmse, prophet_r2score = model.buildProphet(train_data_path, test_data_path)
