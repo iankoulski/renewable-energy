deee165b73f68d4a418a7c98bb0b4857f748a888
Vangelis Koukis <vkoukis@arrikto.com>
2020-10-25 15:13:50 +0000

    Add decorators to transform Python code into Kubeflow Pipeline

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import sys
 import os
+from kale.sdk import pipeline, step
 
+@pipeline( name = 'renewable-energy-pipeline',
+           experiment = 'renewable-energy',
+           autosnapshot = True )
 def renewable_energy_pipeline(wd = '.', wrangle_cache = True, preprocess_cache = True, train_pct = 70):
     data_path = wrangle_step(wd, wrangle_cache)
     preprocessed_data_path = preprocess_step(data_path, preprocess_cache)
@@ -11,32 +15,38 @@ def renewable_energy_pipeline(wd = '.', wrangle_cache = True, preprocess_cache =
     techniques = rank_models_step(linear_rmse, prophet_rmse, randomforest_rmse)
     prediction = predict_with_linear_and_best_model_step(techniques, preprocessed_data_path)
     visualize_prediction_step(wd, prediction)
-    
+
+@step( name = 'wrangle_data' )
 def wrangle_step(wd, wrangle_cache):
     from data import wrangle
     data_path = wrangle(wd, wrangle_cache)
     return data_path
 
+@step( name = 'preprocess_data' )
 def preprocess_step(data_path, preprocess_cache):
     from data import preprocess
     preprocessed_data_path = preprocess(data_path, preprocess_cache)
     return preprocessed_data_path
 
+@step( name = 'split_dataset' )
 def split_step(preprocessed_data_path, train_pct):
     from data import split
     train_data_path, test_data_path = split(preprocessed_data_path, train_pct)
     return train_data_path, test_data_path
 
+@step( name = 'linear_build')
 def linear_build_step(train_data_path, test_data_path):
     from model import buildLinearModel
     linear_rmse = buildLinearModel(train_data_path, test_data_path)
     return linear_rmse
 
+@step( name = 'prophet_build' )
 def prophet_build_step(train_data_path, test_data_path):
     from model import buildProphet
     prophet_rmse = buildProphet(train_data_path, test_data_path)
     return prophet_rmse
 
+@step( name = 'randomforest_build' )
 def randomforest_build_step(train_data_path, test_data_path):
     from model import buildRandomForestRegression
     randomforest_rmse = buildRandomForestRegression(train_data_path, test_data_path)
@@ -57,17 +67,20 @@ def armodel_build_step(train_data_path, test_data_path):
     armodel_rmse = buildARModel(train_data_path, test_data_path)
     return armodel_rmse
 
+@step( name = 'rank_models')
 def rank_models_step(linear_rmse, prophet_rmse, randomforest_rmse):
     from model import rankModels
     models = {'Linear': linear_rmse, 'Prophet': prophet_rmse, 'RandomForest': randomforest_rmse}    
     ranked_models = rankModels(models)
     return ranked_models
 
+@step( name = 'predict_with_best_and_linear_model' )
 def predict_with_linear_and_best_model_step(techniques, preprocessed_data_path):
     from model import predictWithLinearAndBestModel
     prediction = predictWithLinearAndBestModel(techniques, preprocessed_data_path)
     return prediction
 
+@step( name = 'visualize_prediction' )
 def visualize_prediction_step(wd, prediction):
     from model import visualizePrediction
     visualizePrediction(wd, prediction)
